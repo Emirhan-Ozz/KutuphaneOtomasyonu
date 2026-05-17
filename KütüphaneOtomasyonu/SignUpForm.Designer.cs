c9e069a730b8181127b4e5a5356eb509b1dab495
emirhan-ozz <emirhanoz@hotmail.com>
2026-05-17 22:29:25 +0300

    fixed sign up button

--- KütüphaneOtomasyonu/SignUpForm.Designer.cs
+++ KütüphaneOtomasyonu/SignUpForm.Designer.cs
@@ -111,6 +111,7 @@
             this.button1.TabIndex = 6;
             this.button1.Text = "Kayıt Ol";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
